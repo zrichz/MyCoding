413bb1ae3c0727f833aa92aa7f64480ad5fc3946
Richard Haywood <richmugget@yahoo.co.uk>
2025-08-22 14:25:35 +0100

    updated TI to extract single vectors from multi-vector files

---
+++ textual_inversions/run_TI_CHANGER.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Running TI Changer Multiple with virtual environment...
+echo Virtual Environment: C:\MyPythonCoding\MyCoding\image_processors\.venv
+echo.
+"C:\MyPythonCoding\MyCoding\image_processors\.venv\Scripts\python.exe" "TI_CHANGER_MULTIPLE_2024_10_22.py"
+pause
